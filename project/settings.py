898da2ca426df9606269baef7f25cf11c99cba4b
Oleksandr <oleksandrhrynchak02@gmail.com>
2025-11-03 18:54:00 +0200

    Prepare for Render deployment

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '***REMOVED***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bar-chart-sbrm.onrender.com']
 
 
 # Application definition
